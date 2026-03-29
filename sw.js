de6169bea3911a6596deb20cf903459061932d15
pepijnrosmalen2008-droid <pepijn.rosmalen2008@gmail.com>
2026-03-29 23:41:27 +0200

    Bump service worker cache naar v6 (force cache invalidation)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'slagio-v5';
+const CACHE = 'slagio-v6';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo.svg', '/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
