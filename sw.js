147631d016307f0551fd75974a5537e14474bb9a
pepijnrosmalen2008-droid <pepijn.rosmalen2008@gmail.com>
2026-04-30 11:59:35 +0200

    Fix cache: sw v41, quiz scherm bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'slagio-v40';
+const CACHE = 'slagio-v41';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo.svg', '/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
